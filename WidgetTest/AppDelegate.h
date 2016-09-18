ceaf547ca68be1d18069736cbe223fff6855016a
774655048@qq.com <774655048@qq.com>
2016-09-18 10:23:53 +0800

    Initial Commit

---
+++ WidgetTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WidgetTest
+//
+//  Created by YueWen on 16/9/18.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
